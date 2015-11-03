32e2be599143ed82cfa57b8201c5e298ce6d88bc
Timothy <farrar.timothyg@gmail.com>
2015-11-02 21:26:53 -0800

    blogger update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -7,4 +9,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+  
+  end
 end
